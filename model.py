3a5e630f4b7c8f8952ec93689f618138b383cd9c
OMODING RONALD <chidojawbreaker@gmail.com>
2023-02-25 13:52:48 +0300

    commit

--- model.py
+++ model.py
@@ -12,18 +12,18 @@ epochs = 2
 
 weights = models.ResNet18_Weights.DEFAULT
 # Instatiate
-model = models.resnet18(weights=weights)
+pt_model = models.resnet18(weights=weights)
 # Get number of input features into linear layer
-features = model.fc.in_features
+features = pt_model.fc.in_features
 # Modify classifier output shape 
-model.fc = nn.Linear(features, num_classes)
+pt_model.fc = nn.Linear(features, num_classes)
 
-model = model.to(device)
+pt_model = pt_model.to(device)
 
 criterion = nn.CrossEntropyLoss()
 accuracy = train.accuracy_fxn
 
-optimizer = torch.optim.Adam(model.parameters(),lr=0.0001)
+optimizer = torch.optim.Adam(pt_model.parameters(),lr=0.0001)
 scheduler = torch.optim.lr_scheduler.StepLR(optimizer,step_size=7,gamma=0.225)
 
-results,model = train.train_model(epochs,model,data.train_loader,data.val_loader,criterion,train.accuracy_fxn,optimizer,scheduler,device=device)
+results,model = train.train_model(epochs,pt_model,data.train_loader,data.val_loader,criterion,train.accuracy_fxn,optimizer,scheduler,device=device)
